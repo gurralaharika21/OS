6c9561c1f443438af77c9f055cf577bbc6e1316d
gurralaharika21 <gurralaharika21@msitprogram.net>
2020-02-18 11:40:52 +0530

    Day1

---
+++ echo-server/Client.py
@@ -0,0 +1,10 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 3000))
+
+while True:
+    string = input("->")
+    s.send(bytes(string.encode()))
+    msg = s.recv(1024)
+    print(msg.decode("utf-8"))
